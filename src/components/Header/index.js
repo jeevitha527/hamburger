49b22f8bfaa49555d18346b6501944184821c039
jeevitha527 <122962726+jeevitha527@users.noreply.github.com>
2023-08-09 10:10:36 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import Popup from 'reactjs-popup'
 import {withRouter, Link} from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {IoMdClose} from 'react-icons/io'
-import {AiFillHome} from 'react-icons/io'
+import {AiFillHome} from 'react-icons/ai'
 import {BsInfoCircleFill} from 'react-icons/bs'
 
 import './index.css'
